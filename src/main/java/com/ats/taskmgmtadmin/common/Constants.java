e6d2eb6237dce0a7b86dd46d104002681fef3be7
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-08-21 11:29:12 +0530

    /Akshay

--- src/main/java/com/ats/taskmgmtadmin/common/Constants.java
+++ src/main/java/com/ats/taskmgmtadmin/common/Constants.java
@@ -4,19 +4,27 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import org.springframework.http.client.support.BasicAuthorizationInterceptor;
+import org.springframework.web.client.RestTemplate;
+
 public class Constants {
- 
-	//Web Api Path url
+
+	// Web Api Path url
 	public static final String url = "http://localhost:8095/";
+	public static RestTemplate rest = new RestTemplate();
 	
+	 public static RestTemplate getRestTemplate() {
+			rest=new RestTemplate();
+			rest.getInterceptors().add(new BasicAuthorizationInterceptor("aaryatech", "Aaryatech@1cr"));
+			return rest;
 
+			} 
+	 
+	public static String getCurDateTime() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Calendar cal = Calendar.getInstance();
+		String curDateTime = dateFormat.format(cal.getTime());
+		return curDateTime;
 
-public static String getCurDateTime()
-{
-	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	Calendar cal = Calendar.getInstance();
-	String curDateTime = dateFormat.format(cal.getTime());
-	return curDateTime;
-
-}
+	}
 }
