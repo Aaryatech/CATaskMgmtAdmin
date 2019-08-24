6755dd98306a86ca8b8f0f5b9bdc1ec244283388
ubuntu <ubuntu@ubuntu-Lenovo-ideapad-330-15IKB>
2019-08-24 11:41:29 +0530

    ..Sac

--- src/main/java/com/ats/taskmgmtadmin/common/Constants.java
+++ src/main/java/com/ats/taskmgmtadmin/common/Constants.java
@@ -15,14 +15,15 @@ public class Constants {
 	public static final String url = "http://localhost:8095/";
 	//public static final String url = "http://132.148.143.124:8080/CATaskMngtApi/";
 	public static RestTemplate rest = new RestTemplate();
-	
+	public static final String imageSaveUrl = "/home/lenovo/Downloads/"; public
+	  static String[] values = { "jpg", "jpeg", "gif", "png" };
 	 public static RestTemplate getRestTemplate() {
 			rest=new RestTemplate();
 			rest.getInterceptors().add(new BasicAuthorizationInterceptor("aaryatech", "Aaryatech@1cr"));
 			return rest;
 
 			} 
-	 
+	String imageViewUrl="http://132.148.143.124:8080/uploads/ujwal/UjjwalImg/";
 	public static String getCurDateTime() {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Calendar cal = Calendar.getInstance();
